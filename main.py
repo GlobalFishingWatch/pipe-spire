fd7e582f879f6e7f5473fe8069f370befd56a734
Matias Piano <smpiano@gmail.com>
2022-04-23 20:59:36 -0300

    Adds Spire assets and uses python instead of bash for commands

---
+++ main.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+import sys
+import logging
+
+from pipe_spire.normalization import normalize
+
+logging.basicConfig(level=logging.INFO)
+
+SUBCOMMANDS = {
+    "normalize": normalize,
+}
+
+if __name__ == "__main__":
+    logging.info("Running %s", sys.argv)
+
+    if len(sys.argv) < 2:
+        logging.info("No subcommand specified. Run pipeline [SUBCOMMAND], where subcommand is one of %s", SUBCOMMANDS.keys())
+        exit(1)
+
+    subcommand = sys.argv[1]
+    subcommand_args = sys.argv[2:]
+
+    SUBCOMMANDS[subcommand](subcommand_args)
